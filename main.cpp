76e440649f1348906e1aad48b6914a8654178af5
GirtsRimis <girts60@gmail.com>
2020-12-08 11:42:45 +0200

    add comments

--- main.cpp
+++ main.cpp
@@ -6,39 +6,49 @@ using boost::asio::ip::tcp;
 
 int main(int argc, char* argv[])
 {
-    try
+  try
+  {
+    if (argc != 2)
     {
-        if (argc != 2)
-        {
-            std::cerr << "Usage: ./serial-echo <host> (ex. localhost)\n";
-            return 1;
-        }
-
-        boost::asio::io_context io_context;
-        tcp::resolver resolver(io_context);
-        tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
-        tcp::socket socket(io_context);
-        boost::asio::connect(socket, endpoints);
-
-        // Infinite for loop
-        for (;;)
-        {
-            boost::array<char, 128> buf;
-            boost::system::error_code error;
-
-            size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-            if (error == boost::asio::error::eof)
-                break; // Connection closed cleanly by peer
-            else if (error)
-                throw boost::system::system_error(error); // Other error
-
-            std::cout.write(buf.data(), len);
-        }
+      std::cerr << "Usage: client <host>" << std::endl;
+      return 1;
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+
+    std::cout << "The port: " << argv[1] << std::endl;
+
+    boost::asio::io_context io_context;
+
+    // Create the parameter as an tcp object
+    tcp::resolver resolver(io_context);
+    // Create the endpoints to which connection is established
+    tcp::resolver::results_type endpoints =
+      resolver.resolve(argv[1], "daytime");
     
+    // Create a socket
+    tcp::socket socket(io_context);
+    // Connect to a socket
+    boost::asio::connect(socket, endpoints);
+
+    // To hold the data, which is recieved
+    for (;;)
+    {
+      boost::array<char, 128> buf;
+      boost::system::error_code error;
+
+      size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+      if (error == boost::asio::error::eof)
+        break; // Connection closed cleanly by peer.
+      else if (error)
+        throw boost::system::system_error(error); // Some other error.
+
+      std::cout.write(buf.data(), len);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
 }
