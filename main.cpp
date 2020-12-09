9e6fcb91c8dbc730f38810ba9e294b88471fcda9
GirtsRimis <girts60@gmail.com>
2020-12-09 21:48:00 +0200

    reafactor into aysnc serial device
    Change the sync code into async code using classes and remove some incorrect code.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,88 @@
 #include <iostream>
-#include <boost/array.hpp>
+#include <string>
+#include <boost/bind/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
 
+
 // Sync sertial echo device app 
+
+class SerialConnection : public boost::enable_shared_from_this<SerialConnection>
+{
+private:
+    boost::asio::serial_port serialPort;
+    std::string message;
+
+public:
+    typedef boost::shared_ptr<SerialConnection> pointer;
+
+    static pointer create(boost::asio::io_context& io_context, const char* portName)
+    {
+        return pointer(new SerialConnection(io_context, portName));
+    }
+
+    boost::asio::serial_port& getSerialPort()
+    {
+        return this->serialPort;
+    }
+
+    void start()
+    {
+        this->message = "Testing connection!\n";
+
+        boost::asio::async_write(this->serialPort, boost::asio::buffer(this->message),
+            boost::bind(&SerialConnection::handleWrite, shared_from_this(),
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+
+private:
+    SerialConnection(boost::asio::io_context& io_context, const char* portName)
+        : serialPort(io_context, portName)
+    {
+        ;
+    }
+
+    void handleWrite(const boost::system::error_code&, size_t)
+    {
+        ;
+    }
+};
+
+class SerialServer
+{
+private:
+    boost::asio::io_context& io_context;
+    const char* portName;
+
+public:
+    SerialServer(boost::asio::io_context& io_context, const char* portName)
+        : io_context(io_context)
+        , portName(portName)
+    {
+        start_accept();
+    }
+
+private:
+    void start_accept()
+    {
+        SerialConnection::pointer newConnection = 
+            SerialConnection::create(this->io_context, this->portName);
+    }
+
+    void handleAccept(SerialConnection::pointer newConnection, 
+        const boost::system::error_code& error)
+    {
+        if (!error)
+        {
+            newConnection->start();
+        }
+
+        start_accept();
+    }
+
+};
 
 int main(int argc, char* argv[])
 {
@@ -15,29 +95,7 @@ int main(int argc, char* argv[])
         }
 
         boost::asio::io_context io_context;
-        boost::asio::serial_port port(io_context, argv[1]);
-
-        for (;;)
-        {
-            boost::array<char, 128> buffer;
-            boost::system::error_code error;
-
-            size_t lenght = port.read_some(boost::asio::buffer(buffer), error);
-
-            if (error == boost::asio::error::eof)
-                break; // Connection closed cleanly by peer
-            else if (error)
-                throw boost::system::system_error(error);
-
-            std::cout.write(buffer.data(), lenght);
-
-            port.write_some(boost::asio::buffer(buffer), error);
-
-            if (error == boost::asio::error::eof)
-                break; // Connection closed cleanly by peer
-            else if (error)
-                throw boost::system::system_error(error);
-        }
+        SerialServer serialPort(io_context, argv[1]);
         io_context.run();
     }
     catch(const std::exception& e)
@@ -45,4 +103,5 @@ int main(int argc, char* argv[])
         std::cerr << e.what() << '\n';
     }
     
+    return 0;
 }
