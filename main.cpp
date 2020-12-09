cc2cee01c72af5bc77a664231c842b406bf469ad
GirtsRimis <girts60@gmail.com>
2020-12-09 22:51:34 +0200

    add option to customize baud rate

--- main.cpp
+++ main.cpp
@@ -17,9 +17,9 @@ private:
 public:
     typedef boost::shared_ptr<SerialConnection> pointer;
 
-    static pointer create(boost::asio::io_context& io_context, const char* portName)
+    static pointer create(boost::asio::io_context& io_context, SerialPortInformation& portInformation)
     {
-        return pointer(new SerialConnection(io_context, portName));
+        return pointer(new SerialConnection(io_context, portInformation));
     }
 
     boost::asio::serial_port& getSerialPort()
@@ -29,7 +29,11 @@ public:
 
     void start()
     {
+        // Implement read here
+
         this->message = "Testing connection!\n";
+
+        std::cout << "Writing message: " << this->message << std::endl;
 
         boost::asio::async_write(this->serialPort, boost::asio::buffer(this->message),
             boost::bind(&SerialConnection::handleWrite, shared_from_this(),
@@ -37,11 +41,20 @@ public:
             boost::asio::placeholders::bytes_transferred));
     }
 
-private:
-    SerialConnection(boost::asio::io_context& io_context, const char* portName)
-        : serialPort(io_context, portName)
+    void setupPort(boost::asio::serial_port& serialPort, unsigned int baudRate)
     {
-        ;
+        serialPort.set_option(boost::asio::serial_port_base::baud_rate(baudRate));
+        serialPort.set_option(boost::asio::serial_port_base::character_size(8));
+        serialPort.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+        serialPort.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+        serialPort.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+    }
+
+private:
+    SerialConnection(boost::asio::io_context& io_context, SerialPortInformation& portInformation)
+        : serialPort(io_context, portInformation.portName)
+    {
+        setupPort(serialPort, portInformation.baudRate);
     }
 
     void handleWrite(const boost::system::error_code&, size_t)
@@ -54,21 +67,23 @@ class SerialServer
 {
 private:
     boost::asio::io_context& io_context;
-    const char* portName;
+    SerialPortInformation& portInformation;
 
 public:
-    SerialServer(boost::asio::io_context& io_context, const char* portName)
+    SerialServer(boost::asio::io_context& io_context, SerialPortInformation& portInformation)
         : io_context(io_context)
-        , portName(portName)
+        , portInformation(portInformation)
     {
         start_accept();
     }
 
 private:
     void start_accept()
-    {
+    {           
         SerialConnection::pointer newConnection = 
-            SerialConnection::create(this->io_context, this->portName);
+            SerialConnection::create(this->io_context, portInformation);
+
+        
     }
 
     void handleAccept(SerialConnection::pointer newConnection, 
@@ -81,22 +96,35 @@ private:
 
         start_accept();
     }
-
 };
 
-int main(int argc, char* argv[])
+struct SerialPortInformation
+{
+    std::string portName;
+    unsigned int baudRate;
+};
+
+
+int main()
 {
     try
     {
-        if (argc != 2)
-        {
-            std::cerr << "Usage: ./sertial-echo <port> (ex. /dev/ttyS0)";
-            return 1;
-        }
+        SerialPortInformation portInformation;
+
+        std::cout << "Enter serial port: ";
+        std::cin >> portInformation.portName;
+
+        std::cout << "Enter baud rate: ";
+        std::cin >> portInformation.baudRate;
+
+        std::cout << "Opening port: " << portInformation.portName << std::endl;
 
         boost::asio::io_context io_context;
-        SerialServer serialPort(io_context, argv[1]);
+        SerialServer serialPort(io_context, portInformation);
         io_context.run();
+
+        std::cout << "Closing port\n";
+
     }
     catch(const std::exception& e)
     {
