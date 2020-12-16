4bcda196fc504803f564fc3d394a6d34decf324f
GirtsRimis <girts60@gmail.com>
2020-12-16 21:15:54 +0200

    abstract test code
    This is done to avoid duplication of code and make it more modular/managable

--- test.cpp
+++ test.cpp
@@ -6,27 +6,78 @@
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_suit1)
+struct SerialPortInformation
+{
+    std::string portName;
+    unsigned long baudRate;
+    std::string sendString;
+};
 
-BOOST_AUTO_TEST_CASE(test_less_than_buffer_size)
+class SerialServer
+{
+private:
+    boost::asio::io_context& io_context;
+    SerialPortInformation portInformation;
+
+    boost::asio::serial_port serialPort;
+    boost::array<char, 12> dataBuffer;
+
+public:
+    SerialServer(boost::asio::io_context& io_context, SerialPortInformation portInformation)
+        : io_context(io_context)
+        , portInformation(portInformation)
+        , serialPort(io_context, portInformation.portName)
+    {
+        setupPort(this->serialPort, this->portInformation.baudRate);
+        startReadAndWrite();
+    }
+
+    void startReadAndWrite()
+    {
+        boost::system::error_code error;
+        serialPort.write_some(boost::asio::buffer(this->portInformation.sendString), error);
+        serialPort.read_some(boost::asio::buffer(this->dataBuffer), error);
+    }
+
+    const char* getBufferData() const
+    {
+        return this->dataBuffer.data();
+    }
+
+    void setupPort(boost::asio::serial_port& serialPort, unsigned long baudRate)
+    {
+        serialPort.set_option(boost::asio::serial_port_base::baud_rate(baudRate));
+        serialPort.set_option(boost::asio::serial_port_base::character_size(8));
+        serialPort.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+        serialPort.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+        serialPort.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+    }
+};
+
+void AddBoostProgramOptions(SerialPortInformation& portInformation)
 {
     using namespace boost::program_options;
 
+    const char* HELP = "help";
     const char* PORT = "port";
-
-    try
-    {
-        std::string portName;
+    const char* BAUD_RATE = "baud_rate";
 
         options_description description("Options");
         description.add_options()
-            (PORT, value<std::string>(&portName)->default_value("/dev/ttyS0"), "set serial port")
+            (HELP, "show help message")
+            (PORT, value<std::string>(&portInformation.portName)->default_value("/dev/pts/3"), "set serial port")
+            (BAUD_RATE, value<unsigned long>(&portInformation.baudRate)->default_value(9600), "set baud rate")
         ;
 
         variables_map variableMap;
         store(parse_command_line(boost::unit_test::framework::master_test_suite().argc, boost::unit_test::framework::master_test_suite().argv, description), variableMap);
         notify(variableMap);
 
+        if (variableMap.count(HELP))
+        {
+            std::cout << description << "\n";
+        }
+        
         if (variableMap.count(PORT))
         {
             std::cout << "Serial port device was set to " << variableMap[PORT].as<std::string>() << "\n";
@@ -36,26 +87,39 @@ BOOST_AUTO_TEST_CASE(test_less_than_buffer_size)
             std::cout << "Serial port device was set to default\n";
         }
 
-        boost::asio::io_context io_context;
-        boost::asio::serial_port serialPort(io_context, portName);
+        if (variableMap.count(BAUD_RATE))
+        {
+            std::cout << "Serial port device baud rate was set to " << variableMap[BAUD_RATE].as<unsigned long>() << "\n";
+        }
+        else
+        {
+            std::cout << "Serial device baud rate was set to default\n";
+        }
+}
 
-        serialPort.set_option(boost::asio::serial_port_base::baud_rate(9600));
-        serialPort.set_option(boost::asio::serial_port_base::character_size(8));
-        serialPort.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
-        serialPort.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
-        serialPort.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+void Setup_SerialServer_And_Check_Equal_With_SerialServer_Output(const char* testString)
+{
+    SerialPortInformation portInformation;
+    AddBoostProgramOptions(portInformation);
+    portInformation.sendString = testString;
 
-        boost::array<char, 12> dataBuffer;
-        boost::system::error_code error;
+    boost::asio::io_context io_context;
+    SerialServer serialServer(io_context, portInformation);
 
-        serialPort.write_some(boost::asio::buffer("test_conn"), error);
-        serialPort.read_some(boost::asio::buffer(dataBuffer), error);
+    BOOST_CHECK_EQUAL(serialServer.getBufferData(), testString);
+}
 
-        BOOST_CHECK_EQUAL(dataBuffer.data(), "test_conn");
+BOOST_AUTO_TEST_SUITE(test_suit)
+
+BOOST_AUTO_TEST_CASE(test_less_than_buffer_size)
+{
+    try
+    {
+        Setup_SerialServer_And_Check_Equal_With_SerialServer_Output("test_conn");
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << "[ERROR]: " << e.what() << '\n';
     }
     catch(const boost::system::error_code& e)
     {
@@ -65,52 +129,13 @@ BOOST_AUTO_TEST_CASE(test_less_than_buffer_size)
 
 BOOST_AUTO_TEST_CASE(test_more_than_buffer_size)
 {
-    using namespace boost::program_options;
-
-    const char* PORT = "port";
-
     try
     {
-        std::string portName;
-
-        options_description description("Options");
-        description.add_options()
-            (PORT, value<std::string>(&portName)->default_value("/dev/ttyS0"), "set serial port")
-        ;
-
-        variables_map variableMap;
-        store(parse_command_line(boost::unit_test::framework::master_test_suite().argc, boost::unit_test::framework::master_test_suite().argv, description), variableMap);
-        notify(variableMap);
-
-        if (variableMap.count(PORT))
-        {
-            std::cout << "Serial port device was set to " << variableMap[PORT].as<std::string>() << "\n";
-        }
-        else
-        {
-            std::cout << "Serial port device was set to default\n";
-        }
-
-        boost::asio::io_context io_context;
-        boost::asio::serial_port serialPort(io_context, portName);
-
-        serialPort.set_option(boost::asio::serial_port_base::baud_rate(9600));
-        serialPort.set_option(boost::asio::serial_port_base::character_size(8));
-        serialPort.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
-        serialPort.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
-        serialPort.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
-
-        boost::array<char, 12> dataBuffer;
-        boost::system::error_code error;
-
-        serialPort.write_some(boost::asio::buffer("test_connection"), error);
-        serialPort.read_some(boost::asio::buffer(dataBuffer), error);
-
-        BOOST_CHECK_EQUAL(dataBuffer.data(), "test_connection");
+        Setup_SerialServer_And_Check_Equal_With_SerialServer_Output("test_connection");
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << "[ERROR]: " << e.what() << '\n';
     }
     catch(const boost::system::error_code& e)
     {
@@ -120,52 +145,13 @@ BOOST_AUTO_TEST_CASE(test_more_than_buffer_size)
 
 BOOST_AUTO_TEST_CASE(test_non_ASCII_or_null)
 {
-    using namespace boost::program_options;
-
-    const char* PORT = "port";
-
     try
     {
-        std::string portName;
-
-        options_description description("Options");
-        description.add_options()
-            (PORT, value<std::string>(&portName)->default_value("/dev/ttyS0"), "set serial port")
-        ;
-
-        variables_map variableMap;
-        store(parse_command_line(boost::unit_test::framework::master_test_suite().argc, boost::unit_test::framework::master_test_suite().argv, description), variableMap);
-        notify(variableMap);
-
-        if (variableMap.count(PORT))
-        {
-            std::cout << "Serial port device was set to " << variableMap[PORT].as<std::string>() << "\n";
-        }
-        else
-        {
-            std::cout << "Serial port device was set to default\n";
-        }
-
-        boost::asio::io_context io_context;
-        boost::asio::serial_port serialPort(io_context, portName);
-
-        serialPort.set_option(boost::asio::serial_port_base::baud_rate(9600));
-        serialPort.set_option(boost::asio::serial_port_base::character_size(8));
-        serialPort.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
-        serialPort.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
-        serialPort.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
-
-        boost::array<char, 12> dataBuffer;
-        boost::system::error_code error;
-
-        serialPort.write_some(boost::asio::buffer("tēst_\0čonn"), error);
-        serialPort.read_some(boost::asio::buffer(dataBuffer), error);
-
-        BOOST_CHECK_EQUAL(dataBuffer.data(), "tēst_\0čonn");
+        Setup_SerialServer_And_Check_Equal_With_SerialServer_Output("tēst_\0čonn");
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << "[ERROR]: " << e.what() << '\n';
     }
     catch(const boost::system::error_code& e)
     {
