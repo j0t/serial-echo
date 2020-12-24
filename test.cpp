8cb57fcc4c478ce057643cb353c44ba0fbe689c1
GirtsRimis <girts60@gmail.com>
2020-12-24 16:01:20 +0200

    rename variables and functions

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
 
-void make_vector(std::vector<char>& inputVector, const char* data, std::size_t size = -1)
+void makeVector(std::vector<char> &inputVector, const char *data, std::size_t size = -1)
 {
     std::vector<char> createVector;
     inputVector.assign(data, data + size);
@@ -19,30 +19,26 @@ public:
     unsigned long baudRate;
 
 public:
-    SerialPortInformation()
+    SerialPortInformation(int argc, char *argv[])
     {
         using namespace boost::program_options;
 
-        const char* HELP = "help";
-        const char* PORT = "port";
-        const char* BAUD_RATE = "baud_rate";
+        const char *HELP = "help";
+        const char *PORT = "port";
+        const char *BAUD_RATE = "baud_rate";
 
         options_description description("Options");
-        description.add_options()
-            (HELP, "show help message")
-            (PORT, value<std::string>(&this->portName)->default_value("/dev/pts/3"), "set serial port")
-            (BAUD_RATE, value<unsigned long>(&this->baudRate)->default_value(9600), "set baud rate")
-        ;
+        description.add_options()(HELP, "show help message")(PORT, value<std::string>(&this->portName)->default_value("/dev/pts/3"), "set serial port")(BAUD_RATE, value<unsigned long>(&this->baudRate)->default_value(9600), "set baud rate");
 
         variables_map variableMap;
-        store(parse_command_line(boost::unit_test::framework::master_test_suite().argc, boost::unit_test::framework::master_test_suite().argv, description), variableMap);
+        store(parse_command_line(argc, argv, description), variableMap);
         notify(variableMap);
 
         if (variableMap.count(HELP))
         {
             std::cout << description << "\n";
         }
-        
+
         if (variableMap.count(PORT))
         {
             std::cout << "Serial port device was set to " << variableMap[PORT].as<std::string>() << "\n";
@@ -66,14 +62,14 @@ public:
 class SerialServer
 {
 private:
-    boost::asio::io_context& io_context;
+    boost::asio::io_context &io_context;
     SerialPortInformation portInformation;
 
     boost::asio::serial_port serialPort;
     boost::asio::streambuf dataBuffer;
 
 public:
-    SerialServer(boost::asio::io_context& io_context, SerialPortInformation& portInformation)
+    SerialServer(boost::asio::io_context &io_context, SerialPortInformation &portInformation)
         : io_context(io_context)
         , portInformation(portInformation)
         , serialPort(io_context, portInformation.portName)
@@ -81,25 +77,26 @@ public:
         setupPort(this->serialPort, this->portInformation.baudRate);
     }
 
-    void startRead(char endChar)
+    void readData(char endChar, std::vector<char> &inputVector)
     {
         boost::system::error_code error;
         boost::asio::read_until(this->serialPort, this->dataBuffer, endChar, error);
+        getBufferData(inputVector);
     }
 
-    void startWrite(std::vector<char>& sendString)
+    void writeData(std::vector<char> &sendString)
     {
         boost::system::error_code error;
         boost::asio::write(this->serialPort, boost::asio::buffer(sendString, sendString.size()), error);
     }
 
-    void getBufferData(std::vector<char>& inputVector)
+    void getBufferData(std::vector<char> &inputVector)
     {
         std::istream inputStream(&this->dataBuffer);
         inputVector.assign(std::istreambuf_iterator<char>(inputStream), std::istreambuf_iterator<char>());
     }
 
-    void setupPort(boost::asio::serial_port& serialPort, unsigned long baudRate)
+    void setupPort(boost::asio::serial_port &serialPort, unsigned long baudRate)
     {
         serialPort.set_option(boost::asio::serial_port_base::baud_rate(baudRate));
         serialPort.set_option(boost::asio::serial_port_base::character_size(8));
@@ -118,23 +115,23 @@ public:
 
 public:
     TestSerialServerFixture()
-        : serialServer(this->io_context, this->portInformation)
+        : portInformation(boost::unit_test::framework::master_test_suite().argc, boost::unit_test::framework::master_test_suite().argv)
+        , serialServer(this->io_context, this->portInformation)
     {
     }
 
     ~TestSerialServerFixture() = default;
 
-    void Setup_SerialServer_And_Check_Equal_With_SerialServer_Output(const char* testString, int numberOfChars, char endChar)
+    void CompareEcho(const char *testString, int numberOfChars, char endChar)
     {
         std::vector<char> bufferData, sendString, testDataVector;
-        
+
         sendString.assign(testString, testString + numberOfChars);
 
-        this->serialServer.startWrite(sendString);
-        this->serialServer.startRead(endChar);
-        this->serialServer.getBufferData(bufferData);
+        this->serialServer.writeData(sendString);
+        this->serialServer.readData(endChar, bufferData);
 
-        make_vector(testDataVector, testString, numberOfChars);
+        makeVector(testDataVector, testString, numberOfChars);
 
         BOOST_CHECK_EQUAL_COLLECTIONS(bufferData.begin(), bufferData.end(), testDataVector.begin(), testDataVector.end());
     }
@@ -142,24 +139,24 @@ public:
 
 BOOST_FIXTURE_TEST_SUITE(test_suit, TestSerialServerFixture)
 
-    BOOST_AUTO_TEST_CASE(test_less_than_buffer_size)
-    {
-        test_suit::BOOST_AUTO_TEST_CASE_FIXTURE::Setup_SerialServer_And_Check_Equal_With_SerialServer_Output("test_conn!", 11, '!');
-    }
+BOOST_AUTO_TEST_CASE(test_less_than_buffer_size)
+{
+    CompareEcho("test_conn!", 11, '!');
+}
 
-    BOOST_AUTO_TEST_CASE(test_more_than_buffer_size)
-    {
-        test_suit::BOOST_AUTO_TEST_CASE_FIXTURE::Setup_SerialServer_And_Check_Equal_With_SerialServer_Output("test_connection$", 17, '$');
-    }
+BOOST_AUTO_TEST_CASE(test_more_than_buffer_size)
+{
+    CompareEcho("test_connection$", 17, '$');
+}
 
-    BOOST_AUTO_TEST_CASE(test_null)
-    {
-        test_suit::BOOST_AUTO_TEST_CASE_FIXTURE::Setup_SerialServer_And_Check_Equal_With_SerialServer_Output("tēst_\0čo#", 12, '#');
-    }
+BOOST_AUTO_TEST_CASE(test_null)
+{
+    CompareEcho("tēst_\0čo#", 12, '#');
+}
 
-    BOOST_AUTO_TEST_CASE(test_non_ASCII)
-    {
-        test_suit::BOOST_AUTO_TEST_CASE_FIXTURE::Setup_SerialServer_And_Check_Equal_With_SerialServer_Output("tēst_\x01čo@", 12, '@');
-    }
+BOOST_AUTO_TEST_CASE(test_non_ASCII)
+{
+    CompareEcho("tēst_\x01čo@", 12, '@');
+}
 
 BOOST_AUTO_TEST_SUITE_END()
