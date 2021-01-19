35f121e81ad0205bfc430363f1ed949c693fc406
GirtsRimis <girts60@gmail.com>
2021-01-19 21:26:16 +0200

    clean up main.cpp from boost program options
    A SerialPortInformation consturctor exists which has this funcionality.

--- main.cpp
+++ main.cpp
@@ -8,63 +8,11 @@
 #include "SerialServer.h"
 #include "SerialPortInformation.h"
 
-int main(int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
-    using namespace boost::program_options;
-
-    const char* HELP = "help";
-    const char* PORT = "port";
-    const char* BAUD_RATE = "baud_rate";
-    const char* DEBUG_LEVEL = "debug_level";
-
     try
     {  
-        SerialPortInformation portInformation;
-
-        options_description description("Options");
-        description.add_options()
-            (HELP, "show help message")
-            (PORT, value<std::string>(&portInformation.portName)->default_value("/dev/ttyS0"), "set serial port")
-            (BAUD_RATE, value<unsigned long>(&portInformation.baudRate)->default_value(9600), "set baud rate")
-            (DEBUG_LEVEL, value<unsigned int>(&portInformation.debugLevel)->default_value(0), "set debug level (0 - none, 1 - full)")
-        ;
-
-        variables_map variableMap;
-        store(parse_command_line(argc, argv, description), variableMap);
-        notify(variableMap);
-
-        if (variableMap.count(HELP))
-        {
-            std::cout << description << "\n";
-            return 0;
-        }
-        
-        if (variableMap.count(PORT))
-        {
-            std::cout << "Serial port device was set to " << variableMap[PORT].as<std::string>() << "\n";
-        }
-        else
-        {
-            std::cout << "Serial port device was set to default\n";
-        }
-
-        if (variableMap.count(BAUD_RATE))
-        {
-            std::cout << "Serial port device baud rate was set to " << variableMap[BAUD_RATE].as<unsigned long>() << "\n";
-        }
-        else
-        {
-            std::cout << "Serial device baud rate was set to default\n";
-        }
-
-        if (variableMap.count(DEBUG_LEVEL))
-        {
-            std::cout << "Debug level was set to " << variableMap[DEBUG_LEVEL].as<unsigned int>() << "\n";
-        }
-        else
-        {
-            std::cout << "Debug level was set to default\n";
-        }
+        SerialPortInformation portInformation(argc, argv);
 
         std::cout << "Opening port: " << portInformation.portName << std::endl;
 
