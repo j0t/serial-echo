0029ad6abbbecec51b9b209cec4eab66e959e068
GirtsRimis <girts60@gmail.com>
2021-01-25 08:54:51 -0800

    create global fixture
    This way the buffers get allocated and cleaned up for each test case and
    the buffered output gets shown in syslog and console.
    
    The custom main approach could also work, but the buffer cleanup could
    get really complicated.
    
    Also io_context must be on top of the class variables to avoid any
    errors.

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,4 @@
-// #define BOOST_TEST_TOOLS_UNDER_DEBUGGER
-
 #define BOOST_TEST_MODULE
-#define BOOST_TEST_NO_MAIN
-#define BOOST_TEST_ALTERNATIVE_INIT_API
 
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/parameterized_test.hpp>
@@ -10,12 +6,13 @@
 #include "TestSerialServer.h"
 #include "Logger.h"
 
-class TestSerialServerFixture
+struct TestSerialServerFixture
 {
 public:
+    boost::asio::io_context io_context;
+
     SerialPortInformation portInformation;
     TestSerialServer serialServer;
-    boost::asio::io_context io_context;
 
 public:
     TestSerialServerFixture()
@@ -78,6 +75,25 @@ public:
     }
 };
 
+struct AllocateBuffers
+{
+    std::streambuf* const coutbuf = std::cout.rdbuf();
+    
+    AllocateBuffers()
+    {
+        std::clog.rdbuf(new Log("serial-echo-test", LOG_LOCAL0));
+        teebuf* tee = new teebuf(coutbuf, std::clog.rdbuf());
+
+        std::cout.rdbuf(tee);
+    }
+    ~AllocateBuffers()
+    {
+        std::cout.rdbuf(coutbuf);
+    }
+};
+
+BOOST_GLOBAL_FIXTURE(AllocateBuffers);
+
 BOOST_FIXTURE_TEST_SUITE(test_data_transfer, TestSerialServerFixture)
 
 BOOST_AUTO_TEST_CASE(test_less_than_buffer_size)
@@ -97,7 +113,7 @@ BOOST_AUTO_TEST_CASE(test_null)
 
 BOOST_AUTO_TEST_CASE(test_non_ASCII)
 {
-    CompareEcho("tēst_\x01čo@");
+    CompareEcho("tēst_\x01č@");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -114,22 +130,3 @@ BOOST_AUTO_TEST_CASE(test_CTS)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
-
-void make_use_of(char**)
-{
-  std::cout << "Using custom entry point..." << std::endl;
-}
-
-int main(int argc, char* argv[], char* envp[])
-{
-    std::streambuf * const coutbuf = std::cout.rdbuf();
-    std::clog.rdbuf(new Log("serial-echo-test", LOG_LOCAL0));
-
-    teebuf tee(coutbuf, std::clog.rdbuf());
-    std::cout.rdbuf(&tee);
-
-    make_use_of(envp);
-
-    std::cout.rdbuf(coutbuf);
-    return boost::unit_test::unit_test_main(init_unit_test, argc, argv);
-}
